3a086de2d2d881eeae061e4726c8221164cf5e46
Rabbit <166239200+RabbitAsakura@users.noreply.github.com>
2026-02-01 20:16:44 -0500

    Add files via upload

---
+++ src/pipe_demo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+    int fd[2];
+    if (pipe(fd) == -1) { perror("pipe"); exit(1);}
+
+    pid_t pid = fork();
+
+    if(pid < 0) {perror("fork"); exit(1);}
+    if(pid == 0)
+    {
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        execlp("wc", "wc", "-l", (char *)NULL);
+        perror("execlp wc");
+        _exit(1);
+    }
+    else
+    {
+        close(fd[0]);
+        FILE *out = fdopen(fd[1], "w");
+        for(int i = 0; i < 5; ++i)
+        {
+            fprintf(out, "line %d\n", i);
+            fclose(out);
+            waitpid(pid, NULL, 0);
+        }
+    }
+    return 0;
+}
